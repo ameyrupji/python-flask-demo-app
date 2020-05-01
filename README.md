c1345f35eb66a924b69efd053ed093e096b7b823
Amey Rupji <ameyrupji@gmail.com>
2020-04-30 22:42:10 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# python-flask-demo-app
+
+Simple Demo App using Flask!. I have followed [this minimal app](https://flask.palletsprojects.com/en/1.1.x/quickstart/#a-minimal-application) documentation on the Flask website.
---
+++ src/flaskr/routes.py
@@ -0,0 +1,15 @@
+# from .app import create_app
+#
+# app = create_app()
+#
+# def register_routes():
+#     @app.route('/')
+#     def hello_world():
+#         return 'Hello, World!'
+#
+#     @app.route('/api')
+#     def hello_world_api():
+#         return {
+#             'message': 'Hello, World!'
+#         }
+#
